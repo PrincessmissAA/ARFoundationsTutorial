5156447c6f016a36c473ef926aa610c6a1b20d89
PrincessmissAA <73598180+PrincessmissAA@users.noreply.github.com>
2025-02-02 02:36:59 -0700

    added in unity project

---
+++ Assets/Scripts/TrackedImageInfo.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class TrackedImageInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            ListAllImages();
+        }
+    }
+
+    [SerializeField]
+    ARTrackedImageManager m_TrackedImageManager;
+
+    void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
+
+    void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
+
+    void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var newImage in eventArgs.added)
+        {
+            // Handle added event
+        }
+
+        foreach (var updatedImage in eventArgs.updated)
+        {
+            // Handle updated event
+        }
+
+        foreach (var removedImage in eventArgs.removed)
+        {
+            // Handle removed event
+        }
+    }
+
+    void ListAllImages()
+    {
+        Debug.Log(
+            $"There are {m_TrackedImageManager.trackables.count} images being tracked.");
+
+        foreach (var trackedImage in m_TrackedImageManager.trackables)
+        {
+            Debug.Log($"Image: {trackedImage.referenceImage.name} is at " +
+                      $"{trackedImage.transform.position}");
+        }
+    }
+}
